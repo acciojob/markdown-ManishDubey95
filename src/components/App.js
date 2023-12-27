906b585934f149512f5682645f3d2af9dacf216f
ManishDubey95 <136079775+ManishDubey95@users.noreply.github.com>
2023-12-27 11:48:24 +0000

    last commit

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ const App = () => {
   const [typeText, setTypeText] = useState("");
 
   useEffect(() => {
-    setTypeText("#");
+    setTypeText("# ");
   }, []);
 
   return (
--- src/components/Preview.js
+++ src/components/Preview.js
@@ -2,21 +2,11 @@
 import React from "react";
 
 const Preview = ({ typeText }) => {
-  // Split the text based on newline characters
-  const lines = typeText.split('\n');
-
-  // Find the first line that starts with '#' and extract the content
-  const contentAfterHash = lines.find(line => line.trim().startsWith('#'));
-
-  // Display the extracted content without any markdown formatting
-  const displayedText = contentAfterHash ? contentAfterHash.replace(/^#\s*/, '') : typeText;
-
   return (
     <div className="preview">
-      <h1 className="loading">{displayedText}</h1>
+      <h1 className="loading">{typeText.slice(1)}</h1>
     </div>
   );
 };
 
 export default Preview;
-
